058b780bc1776c6cc37276f2e081befe515ed4fb
s183983 <s183983@student.dtu.dk>
2022-03-23 12:33:59 +0100

    Update augmentor.py

--- pytorch/picasso/augmentor.py
+++ pytorch/picasso/augmentor.py
@@ -237,10 +237,10 @@ class Augment:
         sinval = torch.sin(angle)
         val0 = torch.zeros_like(cosval)
         val1 = torch.ones_like(cosval)
-        R = torch.stack([val1, val0, val0,
-                         val0, cosval, -sinval,
-                         val0, sinval, cosval], dim=1)
-        R = torch.reshape(R, (3, 3))
+        R = torch.tensor([[val1, val0, val0],
+                         [val0, cosval, -sinval],
+                         [val0, sinval, cosval]])
+        # R = torch.reshape(R, (3, 3))
         return R
 
     @staticmethod
@@ -249,10 +249,11 @@ class Augment:
         sinval = torch.sin(angle)
         val0 = torch.zeros_like(cosval)
         val1 = torch.ones_like(cosval)
-        R = torch.stack([cosval, val0, sinval,
-                         val0, val1, val0,
-                        -sinval, val0, cosval], dim=1)
-        R = R.view(3, 3)
+        R = torch.tensor([[cosval, val0, sinval],
+                         [val0, val1, val0],
+                         [-sinval, val0, cosval]])
+        
+        # R = R.view(3, 3)
         return R
 
     @staticmethod
@@ -261,8 +262,9 @@ class Augment:
         sinval = torch.sin(angle)
         val0 = torch.zeros_like(cosval)
         val1 = torch.ones_like(cosval)
-        R = torch.stack([cosval, -sinval, val0,
-                         sinval, cosval, val0,
-                         val0, val0, val1], dim=1)
-        R = R.view(3, 3)
+        R = torch.tensor([[cosval, -sinval, val0],
+                         [sinval, cosval, val0],
+                         [val0, val0, val1]])
+        
+        # R = R.view(3, 3)
         return R
