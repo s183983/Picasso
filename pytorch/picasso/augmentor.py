64da67f3e6cc021595ecf5424b858ce54872a80e
s183983 <s183983@student.dtu.dk>
2022-03-23 12:21:00 +0100

    Update augmentor.py

--- pytorch/picasso/augmentor.py
+++ pytorch/picasso/augmentor.py
@@ -39,12 +39,12 @@ class Augment:
         """
         if torch.rand([])<prob:
             angle = torch.randn([3])*angle_sigma
-            print("angle")
-            print(angle.shape)
-            print(angle)
-            Rx = cls.rot_x([angle[0]])
-            Ry = cls.rot_y([angle[1]])
-            Rz = cls.rot_z([angle[2]])
+            # print("angle")
+            # print(angle.shape)
+            # print(angle)
+            Rx = cls.rot_x(angle[0])
+            Ry = cls.rot_y(angle[1])
+            Rz = cls.rot_z(angle[2])
             R = torch.matmul(Rz, torch.matmul(Ry, Rx))
             xyz = torch.matmul(xyz, R)
         return xyz
